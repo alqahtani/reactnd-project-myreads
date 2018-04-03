0108dcf5b0a877873f2fb1d5ef0393ae1f21f1c5
Ahmed AlQahtani <alq.ahmad@gmail.com>
2018-04-03 12:20:33 +0300

    Fix: move clearing results from button to component lifecycle to support browser back/forward buttons

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,40 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import Book from './Book'
 
-const SearchList = (props) => {
-  const { 
-    query, 
-    searchBooksList,
-    onSearch,
-    onUpdateReadingStatus,
-    onClearQuery 
-  } = props
+class SearchList extends Component {
+  componentWillMount = () => {
+    this.props.onClearQuery()
+  }
 
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link className="close-search" to='/' onClick={onClearQuery}>Close</Link>
-        <div className="search-books-input-wrapper">
+  render () {
+    const { 
+      query, 
+      searchBooksList,
+      onSearch,
+      onUpdateReadingStatus,
+    } = this.props
 
-          <input type="text" placeholder="Search by title or author" value={query} onChange={(e) => onSearch(e.target.value)} />
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
 
+            <input type="text" placeholder="Search by title or author" value={query} onChange={(e) => onSearch(e.target.value)} />
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {searchBooksList.length > 0 && 
+              searchBooksList.map(book => (
+                <li key={book.id}>
+                  <Book book={book} onUpdateReadingStatus={(book, shelf) => onUpdateReadingStatus(book, shelf)} />
+                </li>
+            ))}
+          </ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {searchBooksList.length > 0 && 
-            searchBooksList.map(book => (
-              <li key={book.id}>
-                <Book book={book} onUpdateReadingStatus={(book, shelf) => onUpdateReadingStatus(book, shelf)} />
-              </li>
-          ))}
-        </ol>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 SearchList.propTypes = {
